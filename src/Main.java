221598987cec7b9b457e53d48cafc512afe99dbd
yuliya pinus <pinus.yulia@ya.ru>
2023-01-23 00:11:35 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 550; // Price of airplane ticket
+        int priceOfMile = 20; // Price of one bonus mile
+
+        int countMiles = ticket / priceOfMile; // Count bonus miles
+        System.out.println(countMiles);
+    }
+}
